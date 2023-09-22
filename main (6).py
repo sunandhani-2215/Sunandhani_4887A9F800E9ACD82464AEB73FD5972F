79c2480be4dd3ef06d1ad3dfd222e55f1a0ade7e
sunandhani-2215 <144523634+sunandhani-2215@users.noreply.github.com>
2023-09-22 12:59:32 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,29 @@
+'''Implement a class called Player that represents cricket player.The Player class should have a
+method called play() which prints "The Player is playing cricket. Derive two classes, Batsman and
+Bowler, from the Player class. Override the play() method in each derived class to print "The batsman
+is batting" and " The Bowler is bowling", respectively. Write a program to create objects of both the 
+Batsman and Bowler classes and call the play() method for each object.'''
+
+
+#Define the base class Player 
+class Player:
+    def play(self):
+      print("The Player is playing cricket.")
+
+#Define the derived class Batsman 
+class Batsman(Player):
+    def play(self):
+      print("The Batsman is batting.")
+
+#Define the derived class Bowler
+class Bowler(Player):
+    def play(self):
+      print("The Bowler is bowling.")
+      
+#Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+#Call the play() method for each object 
+batsman.play()
+bowler.play()
